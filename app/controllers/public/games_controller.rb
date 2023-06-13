cb929882d2f39f058ba4206af4473f266d482aa9
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-13 09:06:54 +0000

    remake10

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,49 +1,50 @@
 class Public::GamesController < ApplicationController
-  
+
   def show
    @gane = Game.find(params[:id])
    @comment = Commemt.new
    @comments = Comment.all.page(params[:page])
   end
-  
+
   def index
    @games = Game.all.page(params[:page])
    @game = Game.new
   end
- 
+
  def create
   @game = Game.new(game_params)
-  @game.member_id = current_member.id
+  @game.user_id = current_user.id
   if @game.save
-   redirect_to game_path(@game), nothice: "投稿完了."
+   format.html { redirect_to @game,nothice: "新規投稿完了."}
+   format.json { render :show, status: :created, location: @game}
   else
-   @games = Gmame.all
-   render 'index'
+  format.html{render 'index'}
+   format.json{render json: @game.errors, status: :unprocessable_entity}
   end
-      
+
  end
- 
+
  def edit
-   
+
  end
- 
+
  def update
    if @game.update(game_params)
      redirect_to game_path(@game), notice: "投稿完了."
    else
     render "edit"
    end
- end 
- 
+ end
+
  def destroy
   @game.destro
   redirect_to game_path
  end
- 
+
  private
- 
+
  def game_params
-     params.require(:game).permit(:tittle, :body)
+     params.require(:game).permit(:tittle,:content,:image,:status, :body)
  end
   def ensure_correct_user
     @game = Game.find(params[:id])
