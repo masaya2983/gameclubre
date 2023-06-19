f4d9edd2804886221550c1c2dede33358e359dbe
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-19 13:48:26 +0000

    コメント

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,26 @@
 class Public::FavoritesController < ApplicationController
-  
-   def create
-     game = Game.find(prams[:game_id])
-     @favorite = current_member.favorites.new(game_id: game.id)
-     @favorite.save
-      render 'replace_btn'
-   end
-  
+
+  def create
+   game = Game.find(prams[:game_id])
+   @favorite = current_member.favorites.new(game_id: game.id)
+   @favorite.save
+   render 'replace_btn'
+  end
+
   def destroy
    game = Game.find(prams[:game_id])
    @favorite = current_member.favorites.find_by(game_id: game.id)
    @favorite.destroy
     render 'replace_btn'
-  end  
+  end
+  #フォロー一覧
+  def followings
+   user = User.find(params[:user_id])
+   @users = user.followings
+  end
+  #フォロワー一覧
+  def followers
+   user = User.find(params[:user_id])
+   @users = user.followers
+  end
 end
