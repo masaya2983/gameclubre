255e239ab970e9ffeb4857f0a8420a91d80834fc
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-13 13:29:07 +0000

    verup

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,9 @@ Rails.application.routes.draw do
    root :to => "homes#top"
    get "home/about"=>"homes#about"
   resources :users, only: [:index,:show,:edit,:update] do
-     resource :relationships, only: [:create, :destroy]
+    get "check" => "customers#check"
+    patch "withdrawal" => "users#withdrawal", as: 'withdrawl'
+    resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
